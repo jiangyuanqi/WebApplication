8388dd6c874d7cd1d6f839e88266ae4295e03f0d
Yuanqi Jiang <yj99@njit.edu>
2019-12-09 14:12:38 -0500

    BlogpostView.py

--- blog_api/src/views/BlogpostView.py
+++ blog_api/src/views/BlogpostView.py
@@ -1,10 +1,8 @@
 #/src/views/BlogpostView.py
-from flask import request, g, Blueprint, json, Response
-from ..shared.Authentication import Auth
-from ..models.BlogpostModel import BlogpostModel, BlogpostSchema
-
-blogpost_api = Blueprint('blogpost_api', __name__)
-blogpost_schema = BlogpostSchema()
+# app initiliazation
+#####################
+# existing code remain #
+######################
 
 
 @blogpost_api.route('/', methods=['POST'])
@@ -13,23 +11,30 @@ def create():
   """
   Create Blogpost Function
   """
-  req_data = request.get_json()
-  req_data['owner_id'] = g.user.get('id')
-  data, error = blogpost_schema.load(req_data)
-  if error:
-    return custom_response(error, 400)
-  post = BlogpostModel(data)
-  post.save()
-  data = blogpost_schema.dump(post).data
+  # app initiliazation
+  #####################
+  # existing code remain #
+  ######################
   return custom_response(data, 201)
 
+# app initiliazation
+#####################
+# existing code remain #
+######################
 
-def custom_response(res, status_code):
+
+@blogpost_api.route('/<int:blogpost_id>', methods=['DELETE'])
+@Auth.auth_required
+def delete(blogpost_id):
   """
-  Custom Response Function
+  Delete A Blogpost
   """
-  return Response(
-    mimetype="application/json",
-    response=json.dumps(res),
-    status=status_code
-  )
+  post = BlogpostModel.get_one_blogpost(blogpost_id)
+  if not post:
+    return custom_response({'error': 'post not found'}, 404)
+  data = blogpost_schema.dump(post).data
+  if data.get('owner_id') != g.user.get('id'):
+    return custom_response({'error': 'permission denied'}, 400)
+
+  post.delete()
+  return custom_response({'message': 'deleted'}, 204)
